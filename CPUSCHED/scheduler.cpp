167d11cc7692d5cb167595b142d4464da5f654d2
Emily Rick <erick002@odu.edu>
2026-05-03 19:29:12 -0400

    Implemented SJF scheduler

--- CPUSCHED/scheduler.cpp
+++ CPUSCHED/scheduler.cpp
@@ -24,3 +24,48 @@ void runFIFO(vector<Process>& processes) {
         currentTime = p.finish;
     }
 }
+
+void runSJF(vector<Process>& processes) {
+    vector<Process> result;
+    int currentTime = 0;
+
+    while (result.size() < processes.size()) {
+        vector<Process> ready;
+
+        for (auto &p : processes) {
+            bool alreadyDone = false;
+
+            for (auto &r : result) {
+                if (r.id == p.id) {
+                    alreadyDone = true;
+                }
+            }
+
+            if (!alreadyDone && p.arrival <= currentTime) {
+                ready.push_back(p);
+            }
+        }
+
+        if (ready.empty()) {
+            currentTime++;
+            continue;
+        }
+
+        sort(ready.begin(), ready.end(),
+            [](Process a, Process b) {
+                return a.burst < b.burst;
+            }
+        );
+        
+        Process p = ready[0];
+
+        p.start = currentTime;
+        p.finish = currentTime + p.burst;
+
+        currentTime = p.finish;
+
+        result.push_back(p);
+    }
+
+    processes = result;
+}
